3d606726ca35e5cccb5800bd49ac17371216b3ef
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-12-06 21:04:32 -0500

    feat(sidebar): agregar estilos y estructura para el sidebar

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,13 @@
+<div class="sidebar-container">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <span class="fs-4">Gestor de papas</span>
+    </a>
+    
+    <hr>
+    
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+            <div class="sidebar-heading">Crudencio</div>
+        </li>
+    </ul>
+</div>
